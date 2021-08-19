ff49eee1d87740f151f2e15be666db39e562cd48
eHag-FRU <hager10@email.franklin.edu>
2021-08-18 23:23:51 -0400

    Removed references to images that were removed in a previous version. Added a class to make and handle a pause menu.

---
+++ core/src/com/mygdx/game/pauseMenu.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+public class pauseMenu {
+
+        public pauseMenu() {
+            /*TODO - Fill out constructor*/
+        }
+
+
+
+        private class input {
+            /*TODO - Fill out input class to handle pause menu input*/
+        }
+
+}
